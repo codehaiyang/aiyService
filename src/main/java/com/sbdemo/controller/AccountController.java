cd9d2efdf60ce139387d1e6bc34e84a6b944739b
aiycode <346981274@qq.com>
2018-10-26 18:06:33 +0800

    add swager

--- src/main/java/com/sbdemo/controller/AccountController.java
+++ src/main/java/com/sbdemo/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.sbdemo.controller;
 
 import com.sbdemo.bean.Account;
 import com.sbdemo.service.IAccountService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,7 @@ public class AccountController {
     @Autowired
     IAccountService iAccountService;
 
+    @ApiOperation(value = "添加账户",notes = "添加")
     @RequestMapping(value = "/add",method = RequestMethod.POST)
     public String addAccount(@RequestParam(value = "name") String name,
                              @RequestParam(value = "money") double money){
@@ -30,6 +33,7 @@ public class AccountController {
 
     }
 
+    @ApiOperation(value = "修改账户",notes = "修改")
     @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
     public  String updateAccount(@PathVariable("id")int id ,
                                  @RequestParam(value = "name",required = true) String name,
@@ -46,11 +50,13 @@ public class AccountController {
         }
     }
 
+    @ApiOperation(value = "查询账户",notes = "查询")
     @RequestMapping(value = "/list",method = RequestMethod.GET)
     public List<Account> getAccounts(){
         return iAccountService.findAccountList();
     }
 
+    @ApiOperation(value = "根据ID查询账户",notes = "根据ID查询")
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public  Account getAccountById(@PathVariable("id") int id){
         return iAccountService.findAccountById(id);
